a05956a78009a7d630842d3b734655bffdc9db51
Henrique <henrique.ph935@gmail.com>
2023-09-09 19:06:10 -0300

    Criar rota funcao e blade do fornecedor

--- routes/web.php
+++ routes/web.php
@@ -23,13 +23,11 @@ Route::middleware('autenticacao:padrao, visitante')->prefix('/app')->group(funct
     Route::get('/sair', 'LoginController@sair')->name('app.sair');
     Route::get('/cliente', 'ClienteController@index')->name('app.cliente');
     Route::get('/fornecedor', 'FornecedorController@index')->name('app.fornecedor');
+    Route::post('/fornecedor/listar', 'FornecedorController@listar')->name('app.fornecedor.listar');
     Route::get('/produto', 'ProdutoController@index')->name('app.produto');
-
 });
 
 Route::get('/teste/{p1}/{p2}', 'TesteController@store')->name('teste');
-
-
 Route::fallback(function(){
     echo 'A rota acessada não existe. <a href="'.route('site.index').'">clique aqui</a> para ir para página inicial';
 });
